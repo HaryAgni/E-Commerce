b92080a5902d8e9c2ae1e6d83ae4b85a38656463
Harshit Agnihotri <haryagni123@gmail.com>
2024-06-30 22:55:49 +0530

    URL changes

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -8,7 +8,7 @@ const About = () => {
     name: "Harshit's Superstore : Your trusted partner ",
     content:
       "At Harshit's Superstore, we are passionate about providing customers with a seamless shopping experience. Our mission is to offer a diverse selection of high-quality products at competitive prices. With a commitment to excellent customer service and satisfaction, we strive to build lasting relationships with our valued patrons. Discover the story behind our brand and join us on our journey to redefine online shopping.",
-    img: "/images/about.jpg",
+    img: "images/about.jpg",
   };
   return (
     <>
